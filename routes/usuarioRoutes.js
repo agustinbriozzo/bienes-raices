34a9d906bc4cbf44ec23015bd99819a8ec249fff
Agustin Briozzo <agustinbriozzo1997@gmail.com>
2026-01-06 13:46:09 -0300

    implementar controllers

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
+import {
+  formularioLogin,
+  formularioRegistro,
+} from "../controllers/usuarioController.js";
 
 const router = express.Router();
 
-router.get("/login", (req, res) => {
-  res.render("auth/login", {
-    autenticado: false,
-  });
-});
+router.get("/login", formularioLogin);
+router.get("/registro", formularioRegistro);
 
 export default router;
