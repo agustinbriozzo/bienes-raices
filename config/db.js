eba4270f150dc47b6a17b78e7a39743ad680a7e6
Agustin Briozzo <agustinbriozzo1997@gmail.com>
2026-01-12 17:46:52 -0300

    conectar app a base de datos

---
+++ config/db.js
@@ -0,0 +1,24 @@
+import { Sequelize } from "sequelize";
+
+const db = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS ?? "",
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT ?? 3306,
+    dialect: "mysql",
+    define: {
+      timestamps: true,
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+    //logging: false,
+  }
+);
+
+export default db;
--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+import "dotenv/config";
 import express from "express";
 import usuarioRoutes from "./routes/usuarioRoutes.js";
+import db from "./config/db.js";
 
 const app = express();
+
+try {
+  await db.authenticate();
+  console.log("Base de datos conectada");
+} catch (error) {
+  console.log(error);
+}
 
 app.set("view engine", "pug");
 app.set("views", "./views");
